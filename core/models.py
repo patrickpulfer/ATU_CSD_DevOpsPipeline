c805ba7e660ebc7d6f7bf924f415253e25d1cbe5
Patrick Pulfer <Patrick.Pulfer1@gmail.com>
2023-04-26 19:41:04 +0100

    Feature: Extend user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    
+    class Role(models.TextChoices):
+        USER = 'User', _('User')
+        AGENT = 'Agent', _('Support Agent')
+        ADMIN = 'Admin', _('Administrator')
+        
+
+    user = models.OneToOneField(
+        User, related_name='profile', on_delete=models.CASCADE)
+    role = models.CharField(
+        choices=Role.choices,
+        max_length=5,
+        )
