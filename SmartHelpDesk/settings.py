38547f084ef66723091f4aeb9cdcc676f5e47502
Patrick Pulfer <Patrick.Pulfer1@gmail.com>
2023-04-26 19:51:34 +0100

    Plugin: Adding Django All-Auth plugin

--- SmartHelpDesk/settings.py
+++ SmartHelpDesk/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core',
 ]
 
@@ -63,10 +66,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
 ]
+
+
+SITE_ID = 1
+
 
 WSGI_APPLICATION = 'SmartHelpDesk.wsgi.application'
 
@@ -98,6 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 
