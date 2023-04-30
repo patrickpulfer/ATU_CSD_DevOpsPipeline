f54f98b4a0cbf464c6e9ea6562c85e6c9966ca5d
Patrick Pulfer <Patrick.Pulfer1@gmail.com>
2023-04-30 23:00:05 +0100

    Feature: Search page to allow creation of SR

--- portal/models.py
+++ portal/models.py
@@ -22,7 +22,7 @@ class Ticket(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField()
     status = models.CharField(choices=Status.choices, max_length=11)
-    priority = models.CharField(choices=Status.choices, max_length=11)
+    priority = models.CharField(choices=Priority.choices, max_length=11)
     created_at = models.DateTimeField(auto_created=True)
     updated_at = models.DateTimeField(auto_now=True)
 
